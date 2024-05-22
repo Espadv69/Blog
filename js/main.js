c89035bba2398d69027b1363286397ff95489422
aitor <aitorjob69@gmail.com>
2024-05-22 10:26:20 +0200

    feat: Add auto resizing functionality to comment textarea

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// FOR COMENTARIO
+const textarea = document.getElementById('myTextarea');
+textarea.addEventListener('input', autoResize, false);
+
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+}
