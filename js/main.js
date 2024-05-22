546cbea20c07bd9911d36c5b94c6ef0c40909be8
aitor <aitorjob69@gmail.com>
2024-05-22 13:16:06 +0200

    feat: Add functionality to submit comments and display them on the page

--- js/main.js
+++ js/main.js
@@ -9,8 +9,31 @@ function autoResize() {
 
 // FOR COMENTARIO USUARIO
 const botonComentario = document.querySelector('#boton');
-botonComentario.addEventListener('click', miComentario, false);
+botonComentario.addEventListener('click', miComentario, true);
 
 function miComentario() {
+
+    const nombre = document.querySelector('.nombre');
+    const nombreValue = nombre.value;
+
+    const comentario = document.querySelector('#myTextarea');
+    const comentarioValue = comentario.value;
+
+    if(isNaN(nombreValue) || isNaN(comentarioValue)) {
     
+        const crearElementoH4 = document.createElement('h4');
+        crearElementoH4.textContent = `${nombreValue} dice`;
+    
+        const crearElementoP = document.createElement('p');
+        crearElementoP.textContent = `${comentarioValue}`;
+    
+        const crearElementoHr = document.createElement('hr');
+    
+        const contenedor = document.querySelector('.contenedorComentarios');
+        contenedor.appendChild(crearElementoH4);
+        contenedor.appendChild(crearElementoP); 
+        contenedor.appendChild(crearElementoHr); 
+     }
 }
+
+console.log(miComentario())
